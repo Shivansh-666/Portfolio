21e5fc54e9b85cbc28c9472a13824b798ced0055
shivansh <shivanshnayak555@gmail.com>
2025-01-01 23:16:08 +0530

    DashBoard Screens added

---
+++ src/Screens/DashBoard/MerchStore.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function MerchStore() {
+  return (
+    <View>
+      <Text>MerchStore</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ src/Screens/DashBoard/TermsCondtion.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function TermsCondtion() {
+  return (
+    <View>
+      <Text>TermsCondtion</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
